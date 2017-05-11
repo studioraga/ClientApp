b18c87bee5c32bd0ead77a079a09b8db5dedc489
Raja Mallik <raja.mallik@studioraga.com>
2017-05-11 19:26:56 +0530

    ClientApp: ClientApp is a mobile app which can be used to tweak, control IoT device back end application ( BlinkLed) remotely.
    This app talks to firebase, which in turns passes the config parameter to
    IoT device running backend application (which listens to Firebase).
    
    It is important to configure the authentication and database of firebase
    console before using the ClientApp.
    
    Github:
    https://github.com/studioraga/ClientApp  - Client mobile applicatio
    https://github.com/studioraga/BlinkLed   - IoT backend application.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# ClientApp
+# Client App
+
 ClientApp is a mobile app which can be used to tweak, control IoT device back end application ( BlinkLed) remotely.
+
+This app talks to firebase, which in turns passes the config parameter to IoT device running backend application (which listens to Firebase). 
+
+It is important to configure the authentication and database of firebase console before using the ClientApp.
+
+
+
+
+
---
+++ app/src/main/java/com/ncs/studioraga/blinkled/LightStatus.java
@@ -0,0 +1,22 @@
+package com.ncs.studioraga.blinkled;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+/**
+ * Created by Chokkar on 28/12/2016.
+ */
+
+@IgnoreExtraProperties
+public class LightStatus {
+
+        public int delay;
+
+        // Default constructor required for calls to
+        // DataSnapshot.getValue(User.class)
+        public LightStatus() {
+        }
+
+        public LightStatus(int delay) {
+            this.delay = delay;
+        }
+}
